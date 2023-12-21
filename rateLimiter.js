7e25e34beadf985c78c34951758abffa00429662
Parshwa52 <parshwashahsbmp52@gmail.com>
2023-12-21 11:54:09 +0530

    Increased Rate Limiter to 1 million reqs per minute

--- rateLimiter.js
+++ rateLimiter.js
@@ -1,10 +1,10 @@
 import setRateLimit from "express-rate-limit";
 
-// Rate limit middleware
+// Rate limit middleware for 1 million requests for minute
 const rateLimitMiddleware = setRateLimit({
   windowMs: 60 * 1000,
-  max: 10000,
-  message: "You have exceeded your 10000 requests per minute limit.",
+  max: 1000000,
+  message: "You have exceeded your 1000000 requests per minute limit.",
   headers: true,
 });
 
